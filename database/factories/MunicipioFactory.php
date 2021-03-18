a6ca478fbe4e9de668437a5d02ce1fe255713be1
Pedro Marques <pedromarqueslx@gmail.com>
2021-03-18 17:51:56 +0000

    Factories

--- database/factories/MunicipioFactory.php
+++ database/factories/MunicipioFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Infraestrutura;
+use App\Models\Municipio;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MunicipioFactory extends Factory
@@ -23,53 +23,15 @@ class MunicipioFactory extends Factory
     {
         return [
             'user_id' => $this->faker->randomDigit,
-            'referencia_orait' => $this->faker->randomDigit,
-            'nome_operador' => $this->faker->name,
-            'contribuinte_operador' => $this->faker->randomDigit,
-            'morada_operador' => $this->faker->address,
-            'telefone_operador' => $this->faker->e164PhoneNumber,
-            'email_operador' => $this->faker->companyEmail,
-            'referencia_operador' => $this->faker->randomDigit,
-            'responsavel_operador' => $this->faker->name,
-            'telefone_responsavel_operador' => $this->faker->e164PhoneNumber,
-            'email_responsavel_operador' => $this->faker->companyEmail,
-            'municipio_id' => $this->faker->randomDigit,
-            'municipio_cartas' => $this->faker->randomDigit,
-            'cvp_entrada' => $this->faker->randomDigit,
-            'cvp_entrada_ponto_entrada' => $this->faker->randomDigit,
-            'cvp_entrada_ponto_ligacao' => $this->faker->randomDigit,
-            'cvp_entrada_folga' => $this->faker->randomDigit,
-            'cvp_saida' => $this->faker->randomDigit,
-            'cvp_saida_ponto_entrada' => $this->faker->randomDigit,
-            'cvp_saida_ponto_ligacao' => $this->faker->randomDigit,
-            'cvp_saida_folga' => $this->faker->randomDigit,
-            'tipo_tubo_ponto_entrada' => $this->faker->randomDigit,
-            'tipo_cabo' => $this->faker->randomDigit,
-            'seccao_cabo' => $this->faker->randomDigit,
-            'cabo_identificacao' => $this->faker->randomDigit,
-            'cabo_designacao_cabos' => $this->faker->randomDigit,
-            'cabo_capacidade' => $this->faker->randomDigit,
-            'cabo_peso' => $this->faker->randomDigit,
-            'cabo_diametro' => $this->faker->randomDigit,
-            'cabo_seccao' => $this->faker->randomDigit,
-            'cvp_identificacao_equipamento' => $this->faker->randomDigit,
-            'cvp_cabo_ligacao' => $this->faker->randomDigit,
-            'cvp_tipo_equipamento' => $this->faker->randomDigit,
-            'cvp_dimensoes' => $this->faker->randomDigit,
-            'cvp_peso' => $this->faker->randomDigit,
-            'cvp' => $this->faker->randomDigit,
+            'nome' => $this->faker->city,
+            'email' => $this->faker->companyEmail,
+            'contribuinte' => $this->faker->bankAccountNumber,
+            'telefone' => $this->faker->e164PhoneNumber,
+            'morada' => $this->faker->address,
+            'responsavel' => $this->faker->name,
+            'responsavel_telefone' => $this->faker->e164PhoneNumber,
+            'responsavel_email' => $this->faker->companyEmail,
             'observacoes' => $this->faker->sentence,
-            'metragem' => $this->faker->randomDigit,
-            'quantidade_equipamentos' => $this->faker->randomDigit,
-            'quantidade_pontos_entrada' => $this->faker->randomDigit,
-            'observacoes_resposta' => $this->faker->sentence,
-            'resposta' => $this->faker->randomDigit,
-            //data_resposta_licenciamento' => $this->faker->randomDigit,
-            //'data_pedido_acesso' => $this->faker->name,
-            //'data_resposta_acesso' => $this->faker->name,
-            //'data_pedido_cadastro' => $this->faker->name,
-            //'data_resposta_cadastro' => $this->faker->name,
-
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Infraestrutura;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,8 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //\App\Models\Infraestrutura::factory(10)->create();
-        \App\Models\Municipio::factory(10)->create();
-        //\App\Models\Role::factory(10)->create();
+        //\App\Models\Municipio::factory(10)->create();
+        //\App\Models\Role::factory(2)->create();
         //\App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory(10)->create()->each(function($user){
+                $user->infraestruturas()->save(\App\Models\Infraestrutura::factory()->make());
+        });
+
     }
 }
