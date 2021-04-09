30e41227fc2c4ceea51aec008e93ba0503c90e6f
Pedro Marques <pedromarqueslx@gmail.com>
2021-04-09 16:42:12 +0100

    Concessoes Controller

--- routes/web/concessoes.php
+++ routes/web/concessoes.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function (){
 
     Route::resource('/concessoes','App\Http\Controllers\ConcessoesController');
-    //Route::delete('admin/users/{user}/destroy', 'App\Http\Controllers\UserController@destroy')->name('users.destroy');
 
 });
